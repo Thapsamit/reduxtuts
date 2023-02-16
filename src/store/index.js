e51a3e3b1254dc273d2d024ef002f5f10e9d4efd
Thapsamit <thapliyalamit2001@gmail.com>
2023-02-16 16:58:44 +0530

    used async thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "./slice/userSlice";
+import { userSlice, postsSlice } from "./slice/userSlice";
 
 export const store = configureStore({
   //work like a combined reducer
   reducer: {
     user: userSlice.reducer,
+    posts: postsSlice.reducer,
   },
   devTools: true,
 });
